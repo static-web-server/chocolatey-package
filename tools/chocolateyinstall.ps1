842079dc2ef8894a48626835d867fb0a20639d0f
Jose Quintana <joseluisquintana20@gmail.com>
2022-06-10 23:03:26 +0200

    v2.9.0

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/joseluisq/static-web-server/releases/download/v2.9.0/static-web-server-v2.9.0-i686-pc-windows-msvc.zip'
+$url64      = 'https://github.com/joseluisq/static-web-server/releases/download/v2.9.0/static-web-server-v2.9.0-x86_64-pc-windows-msvc.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'static-web-server*'
+
+  checksum      = '8F3CD9C447136B7DA200099B2265E311085DEBB9F2C2FC30FF8BF732F6EC312C'
+  checksumType  = 'sha256'
+  checksum64    = '72E846329E54C0A794E5C2C62484266F31F0C411BF4243A1BAC58E7C9D697D3D'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyZipPackage @packageArgs
---
+++ update.ps1
@@ -0,0 +1,42 @@
+﻿Import-Module AU
+
+$releases = 'https://github.com/joseluisq/static-web-server/releases'
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyinstall.ps1" = @{
+            "(?i)(^\s*FileFullPath\s*=\s*)(.*)" = "`$1Join-Path `$toolsDir '$($Latest.FileName32)'"
+            "(?i)(^\s*FileFullPath64\s*=\s*)(.*)" = "`$1Join-Path `$toolsDir '$($Latest.FileName64)'"
+        }
+        ".\tools\VERIFICATION.txt" = @{
+            "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+            "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+            "(?i)(\s+x64:).*"            = "`${1} $($Latest.URL64)"
+            "(?i)(checksum64:).*"        = "`${1} $($Latest.Checksum64)"
+        }
+        "static-web-server.nuspec" = @{
+            "\d+\.\d+\.\d+" = "$($Latest.Version)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate {
+    Get-RemoteFiles -Purge -NoSuffix
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $url_base = 'https://github.com'
+    $urls =  $download_page.Links | Where-Object href -match 'pc-windows-msvc' | ForEach-Object href | Select-Object -First 2
+    $url32 = $url_base + $urls.Where({ $_ -match 686 }) 
+    $url64 = $url_base + $urls.Where({ $_ -match 64 })
+    $version = $url32 -split '/' | Select-Object -Last 1 -Skip 1
+
+    @{
+        URL32 = $url32
+        URL64 = $url64
+        Version = $version
+    }
+}
+
+Update-Package -ChecksumFor None
